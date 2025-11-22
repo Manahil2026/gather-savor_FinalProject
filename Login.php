374032d14b2c2f2c5a8f096a2200c56d839741e4
Jeanine Gomez <159081662+jea9gmz@users.noreply.github.com>
2025-11-21 20:41:56 -0500

    Add HTML structure and login form to Login.php

--- Login.php
+++ Login.php
@@ -5,3 +5,47 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+  <title>Gather & Savor | Login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <nav class="main-nav">
+      <div class="logo">
+        <a href="Home.php">Gather & Savor</a>
+      </div>
+      <ul class="nav-links">
+        <li><a href="Home.php">Home</a></li>
+        <li><a href="Recipes.php">Recipes</a></li>
+        <li><a href="meal-planner.php">Meal Planner</a></li>
+        <li><a href="favorites.php">Favorites</a></li>
+        <li><a href="Shopping-list.php">Shopping List</a></li>
+        <li><a href="Login.php" class="active">Login</a></li>
+        <li><a href="Register.php">Register</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main class="page-container auth page">
+    <section class="auth-card">
+      <h1>Login</h1>
+      <form id="login-form" method="post" action="Login.php">
+        <div lass="form-group">
+          <label for="login-email">Email</label>
+          <input type="email" id="lgoin-email" name="email" required>
+        </div>
+
+        <div class="form-group">
+          <label for="login-password">Password</label>
+          <input type="password" id="login-password" name="password" required>
+        </div>
+
+        <button type="submit" class="btn primary-btn">Log In</button>
+      </form>
+      <p class="auth-switch">Don't have an account?<a href="Register.php">Register here.</a></p>
+    </section>
+  </main>
+</body>
+</html>

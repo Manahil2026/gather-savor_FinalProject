c024be920863384f34002d3e59ea037c8479e438
Manahil2026 <manahil.im111@gmail.com>
2025-11-30 11:01:41 -0500

    revert some changes

--- php/recipes/loadMealPlan.php
+++ php/recipes/loadMealPlan.php
@@ -8,20 +8,21 @@ require_once __DIR__ . '/../auth/checkSession.php'; //Make sure user is logged i
 
 $user_id = $_SESSION['user_id']; //Get user id from session
 
+// Load recipe ids for each day
 $stmt = $conn->prepare("SELECT monday, tuesday, wednesday, thursday, friday, saturday, sunday
    FROM meal_plans WHERE user_id = :user LIMIT 1"); // Select all days of the week to display meal plan for the user
 $stmt->execute([':user' => $user_id]);
 
 $meal_plan = $stmt->fetch(PDO::FETCH_ASSOC); //Fetch meal plan as an associative array
 
-return $meal_plan ?: [
-    'monday' => '',
-    'tuesday' => '',
-    'wednesday' => '',
-    'thursday' => '',
-    'friday' => '',
-    'saturday' => '',
-    'sunday' => ''
-]; // return meal plan or empty days if no meal plan is found
+return $meal_plan ?:[
+        'monday' => '',
+        'tuesday' => '',
+        'wednesday' => '',
+        'thursday' => '',
+        'friday' => '',
+        'saturday' => '',
+        'sunday' => ''
+]; 
 
 ?>

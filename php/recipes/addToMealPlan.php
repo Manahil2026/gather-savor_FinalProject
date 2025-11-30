a8220c96f334c1bc636613b9e3df1da14cde73ba
Manahil2026 <manahil.im111@gmail.com>
2025-11-30 10:20:29 -0500

    create add to meal plan functionality

--- php/recipes/addToMealPlan.php
+++ php/recipes/addToMealPlan.php
@@ -5,10 +5,31 @@ require_once __DIR__ . '/../db.php'; //Include db connection file
 require_once __DIR__ . '/../auth/checkSession.php'; // Male sure user is logged in
 
 $user_id = $_SESSION['user_id']; //Get user id from session
+$recipe_id = $_POST['recipe_id']; // Get recipe id from the post request
+$day = $POST['day']; // Get day from the post request
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST'){
-    header('Location: ../../meal-planner.php');
-    exit;
+// Make sure that the day sent in the post request is valid
+$validDays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday, saturday, sunday'];
+if (!in_array($day, $validDays)){
+    die("Invalid day");
 }
+
+// Check if user already has a meal plan row
+
+$stmt = $conn->prepare("SELECT id FROM meal_plans WHERE user_id = ?"); // ? is a placeholder
+$stmt->execute([$user_id]);
+
+if ($stmt->rowCount() === 0){ // If any row does not exist
+    //Create an empty row for the user
+    $conn->prepare("INSERT INTO meal_plans (user_id) VALUES (?)")->execute([$user_id]);
+}
+
+//Update the selected day with recipe_id
+$stmt = $conn->prepare("UPDATE meal_plans SET $day= ? WHERE user_id = ?");
+$stmt->execute([$recipe_id, $user_id]);
+
+//Redirect back to meal planner page
+header("Location: ../../meal-planner.php");
+exit;
 
 ?>

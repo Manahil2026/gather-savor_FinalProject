08836333395a51d77a6147bb955f45674a547a2d
Manahil2026 <manahil.im111@gmail.com>
2025-11-30 08:21:04 -0500

    fix error

--- php/recipes/loadFavorites.php
+++ php/recipes/loadFavorites.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../auth/checkSession.php'; // Make sure user is logged 
 
 $user_id = $_SESSION['user_id']; //Get user id from session
 
-$stmt = $conn->prepare("SELECT recipe_id, recipe_tilte, recipe_image FROM favorites WHERE user_id = :user ORDER BY created_at DESC"); //Select favorite recipes for the user and order by creation date descending which means most recent first
+$stmt = $conn->prepare("SELECT recipe_id, recipe_title, recipe_image FROM favorites WHERE user_id = :user ORDER BY created_at DESC"); //Select favorite recipes for the user and order by creation date descending which means most recent first
 $stmt->execute([':user' => $user_id]);
 
 $favorites = $stmt->fetchAll(PDO::FETCH_ASSOC); //Fetch all favorite recipes as an associative array

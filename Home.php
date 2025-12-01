42e72c22f540d22e65eb3cd8b8ff426e67e30cca
asq2000 <112900148+asq2000@users.noreply.github.com>
2025-12-01 14:10:59 -0500

    Home.php

--- Home.php
+++ Home.php
@@ -1,5 +1,5 @@
 <?php
-require_once = __DIR__ . '/php/auth/checkSession.php'; // Include the session check to make sure user is logged in.
+require_once = __DIR__ . '/php/auth/checkSession.php'); // Include the session check to make sure user is logged in.
 
 // Database connection (adjust to your config)
 try {

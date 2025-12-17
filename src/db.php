163414539b95a656c3ddd9ba9c1517e18d647977
ant <ant@localdomain>
2025-12-17 18:22:48 -0500

    fix path typo

--- src/db.php
+++ src/db.php
@@ -2,9 +2,9 @@
 // This file establishes a connection to the database
 // This will be included via require function in other PHP files that meed database access
 
-require_once "../messages.php";
+require_once "messages.php";
 $dbname = 'gather_savor';
-$username = 'root'; // root is the dafault username for XAMPP
+$username = 'fruity'; // root is the dafault username for XAMPP
 $password = ''; // I will leave this empty because XAMPP's default psswrd is empty
 $dsn = "mysql:host=localhost;dbname=$dbname";
 
